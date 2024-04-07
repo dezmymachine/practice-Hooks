da84c5b04f60ec224dbf293bdbe83779177a4500
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-04-07 17:55:37 +0000

    file and folder setup

---
+++ react-js-practice/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-5xl text-red-800 underline-offset-2 top">
+        Things dey happen for here
+      </h1>
+    </>
+  );
+}
+
+export default App;
